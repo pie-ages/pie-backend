f04f0e0ec80ee7a453a6c2868956c126904ad504
LeoBertoletti <leonardo.bertoletti@gmail.com>
2026-08-12 20:39:23 -0300

    first commit

---
+++ src/test/java/com/ages/pie/PieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ages.pie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
