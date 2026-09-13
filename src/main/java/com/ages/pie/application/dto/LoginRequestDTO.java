109b62407d9b51df5f696cd109c87ec7f281adfa
LeoBertoletti <leonardo.bertoletti@gmail.com>
2026-09-12 23:07:18 -0300

    fix: Removed annotation again

--- src/main/java/com/ages/pie/application/dto/LoginRequestDTO.java
+++ src/main/java/com/ages/pie/application/dto/LoginRequestDTO.java
@@ -1,10 +1,7 @@
 package com.ages.pie.application.dto;
-import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequestDTO(
-    @NotBlank
     String email,
-    @NotBlank
     String password
 ) {
 }
